1542fe493cb8afb0871dbc33302d89bca12829fb
Alexander Obregon <alexander.obregon47@gmail.com>
2022-01-31 16:56:57 -0800

    commit tree ff4d988a5071460a852f78c98fb77cb29bc7eebd

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
+require 'pry'
+
+class Book
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+    def initialize(title = "title")
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
 
 
+
+    end
+
+    def turn_page
+     puts   "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+b1 = Book.new("new")
+# binding.pry
